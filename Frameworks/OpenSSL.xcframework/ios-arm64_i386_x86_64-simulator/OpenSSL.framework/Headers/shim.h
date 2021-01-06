0498d1e14356bd2a39a8a91ed39b7d6ade7e3d41
Richard Lendvai <lendvai.richard@gbsolutions.io>
2021-01-06 22:39:03 +0100

    Update every shim.h

--- Frameworks/OpenSSL.xcframework/ios-arm64_i386_x86_64-simulator/OpenSSL.framework/Headers/shim.h
+++ Frameworks/OpenSSL.xcframework/ios-arm64_i386_x86_64-simulator/OpenSSL.framework/Headers/shim.h
@@ -1,41 +1,108 @@
 #ifndef OpenSSLShim_h
 #define OpenSSLShim_h
 
-#include <openssl/conf.h>
-#include <openssl/evp.h>
-#include <openssl/err.h>
-#include <openssl/bio.h>
-#include <openssl/x509.h>
-#include <openssl/cms.h>
-
-#undef SSL_library_init
-static inline void SSL_library_init() {
-    OPENSSL_init_ssl(0, NULL);
-}
-
-#undef SSL_load_error_strings
-static inline void SSL_load_error_strings() {
-    OPENSSL_init_ssl(OPENSSL_INIT_LOAD_SSL_STRINGS \
-                     | OPENSSL_INIT_LOAD_CRYPTO_STRINGS, NULL);
-}
-
-#undef OpenSSL_add_all_ciphers
-static inline void OpenSSL_add_all_ciphers() {
-    OPENSSL_init_crypto(OPENSSL_INIT_ADD_ALL_CIPHERS, NULL);
-}
-
-#undef OpenSSL_add_all_digests
-static inline void OpenSSL_add_all_digests() {
-    OPENSSL_init_crypto(OPENSSL_INIT_ADD_ALL_DIGESTS, NULL);
-}
-
-#undef OpenSSL_add_all_algorithms
-static inline void OpenSSL_add_all_algorithms() {
-    #ifdef OPENSSL_LOAD_CONF
-    OPENSSL_add_all_algorithms_conf();
-    #else
-    OPENSSL_add_all_algorithms_noconf();
-    #endif
-}
+#include <OpenSSL/aes.h>
+#include <OpenSSL/asn1.h>
+#include <OpenSSL/asn1err.h>
+#include <OpenSSL/asn1t.h>
+#include <OpenSSL/async.h>
+#include <OpenSSL/asyncerr.h>
+#include <OpenSSL/bio.h>
+#include <OpenSSL/bioerr.h>
+#include <OpenSSL/blowfish.h>
+#include <OpenSSL/bn.h>
+#include <OpenSSL/bnerr.h>
+#include <OpenSSL/buffer.h>
+#include <OpenSSL/buffererr.h>
+#include <OpenSSL/camellia.h>
+#include <OpenSSL/cast.h>
+#include <OpenSSL/cmac.h>
+#include <OpenSSL/cms.h>
+#include <OpenSSL/cmserr.h>
+#include <OpenSSL/comp.h>
+#include <OpenSSL/comperr.h>
+#include <OpenSSL/conf.h>
+#include <OpenSSL/conf_api.h>
+#include <OpenSSL/conferr.h>
+#include <OpenSSL/crypto.h>
+#include <OpenSSL/cryptoerr.h>
+#include <OpenSSL/ct.h>
+#include <OpenSSL/cterr.h>
+#include <OpenSSL/des.h>
+#include <OpenSSL/dh.h>
+#include <OpenSSL/dherr.h>
+#include <OpenSSL/dsa.h>
+#include <OpenSSL/dsaerr.h>
+#include <OpenSSL/dtls1.h>
+#include <OpenSSL/e_os2.h>
+#include <OpenSSL/ebcdic.h>
+#include <OpenSSL/ec.h>
+#include <OpenSSL/ecdh.h>
+#include <OpenSSL/ecdsa.h>
+#include <OpenSSL/ecerr.h>
+#include <OpenSSL/engine.h>
+#include <OpenSSL/engineerr.h>
+#include <OpenSSL/err.h>
+#include <OpenSSL/evp.h>
+#include <OpenSSL/evperr.h>
+#include <OpenSSL/hmac.h>
+#include <OpenSSL/idea.h>
+#include <OpenSSL/kdf.h>
+#include <OpenSSL/kdferr.h>
+#include <OpenSSL/lhash.h>
+#include <OpenSSL/md2.h>
+#include <OpenSSL/md4.h>
+#include <OpenSSL/md5.h>
+#include <OpenSSL/mdc2.h>
+#include <OpenSSL/modes.h>
+#include <OpenSSL/obj_mac.h>
+#include <OpenSSL/objects.h>
+#include <OpenSSL/objectserr.h>
+#include <OpenSSL/ocsp.h>
+#include <OpenSSL/ocsperr.h>
+#include <OpenSSL/opensslconf.h>
+#include <OpenSSL/opensslv.h>
+#include <OpenSSL/ossl_typ.h>
+#include <OpenSSL/pem.h>
+#include <OpenSSL/pem2.h>
+#include <OpenSSL/pemerr.h>
+#include <OpenSSL/pkcs12.h>
+#include <OpenSSL/pkcs12err.h>
+#include <OpenSSL/pkcs7.h>
+#include <OpenSSL/pkcs7err.h>
+#include <OpenSSL/rand.h>
+#include <OpenSSL/rand_drbg.h>
+#include <OpenSSL/randerr.h>
+#include <OpenSSL/rc2.h>
+#include <OpenSSL/rc4.h>
+#include <OpenSSL/rc5.h>
+#include <OpenSSL/ripemd.h>
+#include <OpenSSL/rsa.h>
+#include <OpenSSL/rsaerr.h>
+#include <OpenSSL/safestack.h>
+#include <OpenSSL/seed.h>
+#include <OpenSSL/sha.h>
+#include <OpenSSL/srp.h>
+#include <OpenSSL/srtp.h>
+#include <OpenSSL/ssl.h>
+#include <OpenSSL/ssl2.h>
+#include <OpenSSL/ssl3.h>
+#include <OpenSSL/sslerr.h>
+#include <OpenSSL/stack.h>
+#include <OpenSSL/store.h>
+#include <OpenSSL/storeerr.h>
+#include <OpenSSL/symhacks.h>
+#include <OpenSSL/tls1.h>
+#include <OpenSSL/ts.h>
+#include <OpenSSL/tserr.h>
+#include <OpenSSL/txt_db.h>
+#include <OpenSSL/ui.h>
+#include <OpenSSL/uierr.h>
+#include <OpenSSL/whrlpool.h>
+#include <OpenSSL/x509.h>
+#include <OpenSSL/x509_vfy.h>
+#include <OpenSSL/x509err.h>
+#include <OpenSSL/x509v3.h>
+#include <OpenSSL/x509v3err.h>
 
 #endif
